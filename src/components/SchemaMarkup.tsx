4e90c3eda284f248a094957f09f1603afef885bf
sunrisehumancare <info@sunrisehcsllc.com>
2025-03-05 11:30:54 +0530

    Update SchemaMarkup.tsx

--- src/components/SchemaMarkup.tsx
+++ src/components/SchemaMarkup.tsx
@@ -1,79 +1,117 @@
-
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
-const SchemaMarkup: React.FC = () => {
-  const localBusinessSchema = {
-    "@context": "https://schema.org",
-    "@type": "MedicalBusiness",
-    "name": "Sunrise Human Care Services",
-    "image": "https://example.com/photos/sunrise-hcs.jpg",
-    "url": "https://sunrisehcs.com",
-    "telephone": "+18146202162",
-    "address": {
-      "@type": "PostalAddress",
-      "streetAddress": "2050 West Chester Pike",
-      "addressLocality": "Havertown",
-      "addressRegion": "PA",
-      "postalCode": "19083",
-      "addressCountry": "US"
-    },
-    "geo": {
-      "@type": "GeoCoordinates",
-      "latitude": 39.9707,
-      "longitude": -75.3151
-    },
-    "openingHoursSpecification": [
-      {
-        "@type": "OpeningHoursSpecification",
-        "dayOfWeek": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
-        "opens": "09:00",
-        "closes": "17:00"
-      }
-    ],
-    "sameAs": [
-      "https://www.facebook.com/sunrisehcs",
-      "https://twitter.com/sunrisehcs",
-      "https://instagram.com/sunrisehcs"
-    ],
-    "priceRange": "$$",
-    "medicalSpecialty": [
-      "Psychiatry",
-      "Psychology",
-      "Counseling"
-    ],
-    "description": "Experience comprehensive mental health care in Havertown, PA at Sunrise Human Care Services. Our dedicated team of licensed therapists, psychologists, counselors, and psychiatrists provides online and in-person counseling, therapy, psychiatry, and medication management."
-  };
-
-  const healthcareServiceSchema = {
-    "@context": "https://schema.org",
-    "@type": "MedicalTherapy",
-    "name": "Mental Health Therapy",
-    "description": "We specialize in treating anxiety, depression, ADHD, stress, trauma, grief, PTSD, OCD, eating disorders, bipolar disorder, schizophrenia, addiction, substance abuse, and more. We offer individual, couples and family therapy for children, teens, adults, and seniors.",
-    "medicineSystem": "Conventional Medicine",
-    "relevantSpecialty": [
-      "Psychiatry",
-      "Psychology",
-      "Counseling"
-    ],
-    "recognizingAuthority": "American Psychological Association",
-    "study": {
-      "@type": "MedicalObservationalStudy",
-      "studySubject": {
-        "@type": "MedicalEntity",
-        "name": "Mental Health and Wellness"
-      }
+// Local Business (MedicalClinic) Schema
+const localBusinessSchema = {
+  "@context": "https://schema.org",
+  "@type": "MedicalClinic",
+  "@id": "https://sunrisehcsllc.com/#clinic",
+  "name": "Sunrise Human Care Services",
+  "image": {
+    "@type": "ImageObject",
+    "url": "https://sunrisehcsllc.com/photos/sunrise-hcs.jpg",
+    "width": 1200,
+    "height": 630
+  },
+  "url": "https://sunrisehcsllc.com",
+  "telephone": "+18146202162",
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "2050 West Chester Pike",
+    "addressLocality": "Havertown",
+    "addressRegion": "PA",
+    "postalCode": "19083",
+    "addressCountry": "US"
+  },
+  "geo": {
+    "@type": "GeoCoordinates",
+    "latitude": 39.9707,
+    "longitude": -75.3151
+  },
+  "openingHoursSpecification": [
+    {
+      "@type": "OpeningHoursSpecification",
+      "dayOfWeek": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+      "opens": "09:00",
+      "closes": "17:00"
     }
-  };
+  ],
+  "sameAs": [
+    "https://www.facebook.com/sunrisehcs",
+    "https://twitter.com/sunrisehcs",
+    "https://instagram.com/sunrisehcs"
+  ],
+  "priceRange": "$100-$500",
+  "medicalSpecialty": ["Psychiatry", "Psychology", "Counseling"],
+  "description": "Comprehensive mental health care in Havertown, PA at Sunrise Human Care Services.",
+  "aggregateRating": {
+    "@type": "AggregateRating",
+    "ratingValue": "4.8",
+    "reviewCount": 50
+  }
+};
 
+// Healthcare Service Schema
+const healthcareServiceSchema = {
+  "@context": "https://schema.org",
+  "@type": "HealthCareService",
+  "@id": "https://sunrisehcsllc.com/#mental-health-service",
+  "name": "Mental Health Therapy and Psychiatry Services",
+  "description": "Specializing in anxiety, depression, ADHD, PTSD, OCD, bipolar disorder, addiction, and more. Offering individual, couples, and family therapy for all ages in Havertown, PA and via telehealth.",
+  "provider": {
+    "@id": "https://sunrisehcsllc.com/#clinic"
+  },
+  "serviceType": ["Psychiatric", "Therapeutic Procedure", "Mental Health Care"],
+  "specialty": ["Psychiatry", "Psychology", "Counseling"],
+  "availableChannel": [
+    {
+      "@type": "ServiceChannel",
+      "serviceUrl": "https://sunrisehcsllc.com/book-appointment",
+      "availableLanguage": [
+        {
+          "@type": "Language",
+          "name": "English"
+        }
+      ]
+    },
+    {
+      "@type": "ServiceChannel",
+      "serviceUrl": "https://sunrisehcsllc.com/telehealth",
+      "serviceType": "Telemedicine",
+      "availableLanguage": [
+        {
+          "@type": "Language",
+          "name": "English"
+        }
+      ]
+    }
+  ],
+  "areaServed": [
+    {
+      "@type": "Place",
+      "name": "Havertown, PA"
+    },
+    {
+      "@type": "Place",
+      "name": "Online"
+    }
+  ],
+  "offers": {
+    "@type": "Offer",
+    "priceSpecification": {
+      "@type": "PriceSpecification",
+      "priceCurrency": "USD",
+      "minPrice": "100",
+      "maxPrice": "500"
+    }
+  }
+};
+
+const SchemaMarkup = () => {
   return (
     <Helmet>
-      <script type="application/ld+json">
-        {JSON.stringify(localBusinessSchema)}
-      </script>
-      <script type="application/ld+json">
-        {JSON.stringify(healthcareServiceSchema)}
-      </script>
+      <script type="application/ld+json">{JSON.stringify(localBusinessSchema)}</script>
+      <script type="application/ld+json">{JSON.stringify(healthcareServiceSchema)}</script>
     </Helmet>
   );
 };
