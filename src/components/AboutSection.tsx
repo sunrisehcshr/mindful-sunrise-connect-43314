2cf5262fc83ef7b9251c80f294ccaef01ffa28f2
sunrisehumancare <info@sunrisehcsllc.com>
2025-03-07 02:19:11 +0530

    Update AboutSection.tsx

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -20,7 +20,7 @@ const AboutSection: React.FC = () => {
           <div className="order-1 lg:order-2 grid grid-cols-2 gap-4">
             <img className="rounded-2xl h-40 object-cover w-full md:h-64 shadow-md" src="https://images.unsplash.com/photo-1527613426441-4da17471b66d?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=80" alt="Our clinic building" />
             <img className="rounded-2xl h-40 object-cover w-full md:h-64 mt-8 shadow-md" src="https://images.unsplash.com/photo-1551836022-deb4988cc6c0?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=80" alt="Our therapy room" />
-            <img className="rounded-2xl h-40 object-cover w-full md:h-64 shadow-md col-span-2" src="https://i.imgur.com/HrQKRCC.jpeg" />
+            <img className="rounded-2xl h-40 object-cover w-full md:h-64 shadow-md col-span-2" src="https://i.imgur.com/oLQIxo3.jpeg" />
           </div>
         </div>
       </div>
