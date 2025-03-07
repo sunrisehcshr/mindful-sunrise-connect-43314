f50ca018d5bc3dc4e520bcae858cb95e199fa6ad
sunrisehumancare <info@sunrisehcsllc.com>
2025-03-07 05:35:09 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -44,6 +44,7 @@ const Navbar = () => {
     >
       <div className="container mx-auto px-4 md:px-6">
         <div className="flex items-center justify-between">
+          {/* LOGO - Updated to /logo.svg */}
           <Link to="/" className="flex items-center gap-2 group">
             <img
               src="/logo.svg"
