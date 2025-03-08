edbc0e3c91541ac7142814bbf566b7eaba4068a5
sunrisehumancare <info@sunrisehcsllc.com>
2025-03-08 05:50:43 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,12 +11,24 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    target: "esnext",
+    rollupOptions: {
+      treeshake: true,
+      output: {
+        assetFileNames: "assets/[name]-[hash][extname]",
+        chunkFileNames: "assets/[name]-[hash].js",
+        entryFileNames: "assets/[name]-[hash].js",
+      },
+    },
+    minify: "terser",
+    sourcemap: false,
   },
 }));
