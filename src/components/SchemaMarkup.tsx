c9fccaf914924dff5c2d75686b4537ce6efabf5f
sunrisehumancare <info@sunrisehcsllc.com>
2025-03-08 20:13:56 +0530

    Update SchemaMarkup.tsx

--- src/components/SchemaMarkup.tsx
+++ src/components/SchemaMarkup.tsx
@@ -1,50 +1,60 @@
-{
-  "@type": "MedicalClinic",
-  "@id": "https://sunrisehcsllc.com/#clinic",
-  "name": "Sunrise Human Care Services",
-  "url": "https://sunrisehcsllc.com/",
-  "image": {
-    "@type": "ImageObject",
-    "url": "https://sunrisehcsllc.com/photos/sunrise-hcs.jpg",
-    "width": 1200,
-    "height": 630
-  },
-  "address": {
-    "@type": "PostalAddress",
-    "streetAddress": "2050 West Chester Pike",
-    "addressLocality": "Havertown",
-    "addressRegion": "PA",
-    "postalCode": "19083",
-    "addressCountry": "US"
-  },
-  "geo": {
-    "@type": "GeoCoordinates",
-    "latitude": 39.9707,
-    "longitude": -75.3151
-  },
-  "openingHoursSpecification": [
-    {
-      "@type": "OpeningHoursSpecification",
-      "dayOfWeek": [
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday"
-      ],
-      "opens": "09:00",
-      "closes": "17:00"
-    }
-  ],
-  "medicalSpecialty": [
-    "Psychiatry",
-    "Psychology",
-    "Counseling"
-  ],
-  "priceRange": "$100 - $500",
-  "aggregateRating": {
-    "@type": "AggregateRating",
-    "ratingValue": 4.8,
-    "reviewCount": 50
-  }
-}
+import React from "react";
+import { Helmet } from "react-helmet";
+
+const SchemaMarkup = () => {
+  const schemaData = {
+    "@context": "https://schema.org",
+    "@graph": [
+      {
+        "@type": "MedicalClinic",
+        "@id": "https://sunrisehcsllc.com/#clinic",
+        "name": "Sunrise Human Care Services",
+        "url": "https://sunrisehcsllc.com/",
+        "image": {
+          "@type": "ImageObject",
+          "url": "https://sunrisehcsllc.com/photos/sunrise-hcs.jpg",
+          "width": 1200,
+          "height": 630
+        },
+        "address": {
+          "@type": "PostalAddress",
+          "streetAddress": "2050 West Chester Pike",
+          "addressLocality": "Havertown",
+          "addressRegion": "PA",
+          "postalCode": "19083",
+          "addressCountry": "US"
+        },
+        "geo": {
+          "@type": "GeoCoordinates",
+          "latitude": 39.9707,
+          "longitude": -75.3151
+        },
+        "openingHoursSpecification": [
+          {
+            "@type": "OpeningHoursSpecification",
+            "dayOfWeek": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+            "opens": "09:00",
+            "closes": "17:00"
+          }
+        ],
+        "medicalSpecialty": ["Psychiatry", "Psychology", "Counseling"],
+        "priceRange": "$100 - $500",
+        "aggregateRating": {
+          "@type": "AggregateRating",
+          "ratingValue": 4.8,
+          "reviewCount": 50
+        }
+      }
+    ]
+  };
+
+  return (
+    <Helmet>
+      <script type="application/ld+json">
+        {JSON.stringify(schemaData)}
+      </script>
+    </Helmet>
+  );
+};
+
+export default SchemaMarkup;
