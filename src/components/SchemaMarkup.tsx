e105ca1b8d925e56eba4fa98dac8b241bd206da3
sunrisehumancare <info@sunrisehcsllc.com>
2025-03-14 18:27:25 +0530

    Update SchemaMarkup.tsx

--- src/components/SchemaMarkup.tsx
+++ src/components/SchemaMarkup.tsx
@@ -24,7 +24,7 @@ const SchemaMarkup = () => {
         "name": "Sunrise Human Care Services",
         "url": "https://sunrisehcsllc.com/",
         "foundingDate": "2020-01-01",
-        "founder": "John Doe",
+        "founder": "Michael Thevar",
         "logo": {
           "@type": "ImageObject",
           "@id": "https://sunrisehcsllc.com/#logo",
@@ -53,15 +53,58 @@ const SchemaMarkup = () => {
           "postalCode": "19083",
           "addressCountry": "US"
         }
+      },
+      {
+        "@type": "MedicalClinic",
+        "@id": "https://sunrisehcsllc.com/#clinic",
+        "name": "Sunrise Human Care Services - Havertown Mental Health Clinic",
+        "url": "https://sunrisehcsllc.com/",
+        "image": {
+          "@type": "ImageObject",
+          "url": "https://sunrisehcsllc.com/photos/sunrise-hcs.jpg",
+          "width": 1200,
+          "height": 630
+        },
+        "address": {
+          "@type": "PostalAddress",
+          "streetAddress": "2050 West Chester Pike",
+          "addressLocality": "Havertown",
+          "addressRegion": "PA",
+          "postalCode": "19083",
+          "addressCountry": "US"
+        },
+        "geo": {
+          "@type": "GeoCoordinates",
+          "latitude": 39.9707,
+          "longitude": -75.3151
+        },
+        "openingHoursSpecification": [
+          {
+            "@type": "OpeningHoursSpecification",
+            "dayOfWeek": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+            "opens": "09:00",
+            "closes": "17:00"
+          }
+        ],
+        "medicalSpecialty": ["Psychiatry", "Psychology", "Counseling", "Mental Health"],
+        "priceRange": "$100 - $500",
+        "aggregateRating": {
+          "@type": "AggregateRating",
+          "ratingValue": 4.8,
+          "reviewCount": 50
+        },
+        "areaServed": [
+          { "@type": "Place", "name": "Havertown, PA" },
+          { "@type": "Place", "name": "Delaware County, PA" },
+          { "@type": "Place", "name": "Philadelphia Metro Area" }
+        ]
       }
     ]
   };
 
   return (
     <Helmet>
-      <script type="application/ld+json">
-        {JSON.stringify(schemaData)}
-      </script>
+      <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaData) }} />
     </Helmet>
   );
 };
