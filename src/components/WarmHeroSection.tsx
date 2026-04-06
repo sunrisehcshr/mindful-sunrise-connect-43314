f3755fca2ca2d388b4db51d4a61b5a6a5d9d8ddb
sunrisehcshr <sunrisehcshr@users.noreply.github.com>
2026-04-06 17:28:31 +0000

    chore: update home page hero video again

--- src/components/WarmHeroSection.tsx
+++ src/components/WarmHeroSection.tsx
@@ -154,7 +154,7 @@ const WarmHeroSection = () => {
         >
           {shouldLoadVideo && (
             <source
-              src="https://res.cloudinary.com/dabsxebx8/video/upload/q_auto/f_auto/v1775495838/5722168-uhd_3840_2160_24fps_skusi3.mp4"
+              src="https://res.cloudinary.com/dabsxebx8/video/upload/q_auto/f_auto/v1775496433/7123818-hd_2048_1080_30fps_jjiwrk.mp4"
               type="video/mp4"
             />
           )}
